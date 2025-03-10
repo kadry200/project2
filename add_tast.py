c585d3257dba82bc9e2631e001b1585be74dddd7
kadry <km799862@gmail.com>
2025-03-10 15:28:07 +0200

    testing

--- add_tast.py
+++ add_tast.py
@@ -1,10 +1,12 @@
 from add import *
 import pytest
 
-status = app.test_client().get('/').status_code
-data=app.test_client().get('/').data
+def testing ():
 
-assert status==200 , 'invalid'
-assert data== b'<h1 style = "color:red;"> Hello World <h1>'
+  status = app.test_client().get('/').status_code
+  data=app.test_client().get('/').data
+
+  assert status==200 , 'invalid'
+  assert data== b'<h1 style = "color:red;"> Hello World <h1>'
 
 
